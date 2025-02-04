e5cadc20d149ae48ce15cfb85ce5717267cffb72
Muhammad-Haram <haramh643@gmail.com>
2025-02-04 11:56:46 +0500

    create user model

---
+++ backend/models/user.models.js
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+import bycrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    lowercase: true,
+    minLenght: [3, "email must be at least 3 characters long"],
+    maxLenght: [50, "email must be at most 50 characters long"],
+  },
+
+  password: {
+    type: String,
+    required: true,
+    select: false,
+  },
+});
+
+userSchema.statics.hashPassword = async function (password) {
+  return await bycrypt.hash(password, 10);
+};
+
+userSchema.methods.isValidPassword = async function (password) {
+  return await bycrypt.compare(password, this.password);
+};
+
+userSchema.methods.generateJWT = function () {
+  return jwt.sign({ email: this.email }, process.env.JWT_SECRET);
+};
+
+const User = mongoose.model("user", userSchema);
+export default User;
