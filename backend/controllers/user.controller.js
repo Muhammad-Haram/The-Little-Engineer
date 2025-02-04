5f430e7ffafc172178004c88890e58c57ce95ea6
Muhammad-Haram <haramh643@gmail.com>
2025-02-04 18:33:02 +0500

    redis setup

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,6 +1,7 @@
 import userModel from "../models/user.model.js";
 import * as userService from "../services/user.service.js";
 import { validationResult } from "express-validator";
+import redisClient from "../services/redis.service.js";
 
 export const createUserController = async (req, res) => {
   const errors = validationResult(req);
@@ -51,5 +52,5 @@ export const loginController = async (req, res) => {
 
 export const profileController = async (req, res) => {
   console.log(req.user);
-  res.status(200).json({ user: req.user }); 
+  res.status(200).json({ user: req.user });
 };
