7a332a116d0fa0bf5219d3e4981f201166b63719
Muhammad-Haram <haramh643@gmail.com>
2025-02-08 15:42:05 +0500

    project create backend

--- backend/services/project.service.js
+++ backend/services/project.service.js
@@ -4,12 +4,22 @@ export const createProject = async ({ name, userId }) => {
   if (!name) {
     throw new Error("Name is required");
   }
-
   if (!userId) {
-    throw new Error("User ID is required");
+    throw new Error("UserId is required");
   }
 
-  const project = await projectModel.create({ name, users: [userId] });
+  let project;
+  try {
+    project = await projectModel.create({
+      name,
+      users: [userId],
+    });
+  } catch (error) {
+    if (error.code === 11000) {
+      throw new Error("Project name already exists");
+    }
+    throw error;
+  }
 
   return project;
 };
