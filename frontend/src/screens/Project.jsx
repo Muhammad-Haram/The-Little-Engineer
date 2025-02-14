414ac1fdd593b005bda418f00132304ce29375a6
Muhammad-Haram <haramh643@gmail.com>
2025-02-14 17:57:39 +0500

    sidebar ui

--- frontend/src/screens/Project.jsx
+++ frontend/src/screens/Project.jsx
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { useLocation, useNavigate } from "react-router-dom";
 
 const Project = () => {
   const navigate = useNavigate();
   const location = useLocation();
+
+  const [isSidePanelOpen, setIsSidePanelOpen] = useState(false);
 
   console.log(location.state);
 
@@ -17,24 +19,28 @@ const Project = () => {
             <p>Add collaborator</p>
           </button>
 
-          <button className="p-2">
+          <button
+            onClick={() => setIsSidePanelOpen(!isSidePanelOpen)}
+            className="p-2"
+          >
             <i className="ri-group-fill"></i>
           </button>
         </header>
 
         <div className="conversation-area pt-14 pb-10 flex-grow flex flex-col h-full relative">
           <div className="message-box p-2 flex-grow flex flex-col gap-2 overflow-auto max-h-full scrollbar-hide">
-
             <div className="message max-w-64 flex flex-col p-2 bg-slate-50 w-fit rounded-md">
               <small className="opacity-65 text-xs">example@gmail.com</small>
-              <p className="text-sm">Lorem ipsum dolor, sit amet consectetur Lorem ipsum dolor, sit amet consectetur</p>
+              <p className="text-sm">
+                Lorem ipsum dolor, sit amet consectetur Lorem ipsum dolor, sit
+                amet consectetur
+              </p>
             </div>
 
             <div className="message ml-auto max-w-64 flex flex-col p-2 bg-slate-50 w-fit rounded-md">
               <small className="opacity-65 text-xs">example@gmail.com</small>
               <p className="text-sm">Lorem ipsum dolor, sit amet consectetur</p>
             </div>
-
           </div>
 
           <div className="inputField w-full flex absolute bottom-0">
@@ -47,6 +53,23 @@ const Project = () => {
               <i className="ri-send-plane-fill"></i>
             </button>
           </div>
+        </div>
+
+        <div
+          className={`sidePanel w-full h-full flex flex-col gap-2 bg-slate-50 absolute transition-all ${
+            isSidePanelOpen ? "translate-x-0" : "-translate-x-full"
+          } top-0`}
+        >
+          <header className="flex justify-between items-center px-4 p-2 bg-slate-200">
+            <h1 className="font-semibold text-lg">Collaborators</h1>
+
+            <button
+              onClick={() => setIsSidePanelOpen(!isSidePanelOpen)}
+              className="p-2"
+            >
+              <i className="ri-close-fill"></i>
+            </button>
+          </header>
         </div>
       </section>
     </main>
