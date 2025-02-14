e026f6b5186bf773df38e3d65c2c81317d254c9d
Muhammad-Haram <haramh643@gmail.com>
2025-02-14 17:18:51 +0500

    creating message ui

--- frontend/src/screens/Project.jsx
+++ frontend/src/screens/Project.jsx
@@ -10,7 +10,7 @@ const Project = () => {
 
   return (
     <main className="h-screen w-screen flex">
-      <section className="left relative flex flex-col h-screen min-w-96 bg-slate-300">
+      <section className="left relative flex flex-col h-screen min-w-72 bg-slate-300">
         <header className="flex justify-between items-center p-2 px-4 w-full bg-slate-100 absolute z-10 top-0">
           <button className="flex gap-2">
             <i className="ri-add-fill mr-1"></i>
@@ -23,17 +23,29 @@ const Project = () => {
         </header>
 
         <div className="conversation-area pt-14 pb-10 flex-grow flex flex-col h-full relative">
-          <div className="message-box">
-            <div className="inputField w-full flex absolute bottom-0">
-              <input
-                className="p-2 px-4 border-none outline-none flex-grow"
-                type="text"
-                placeholder="Enter message"
-              />
-              <button className="px-5 bg-slate-950 text-white">
-                <i className="ri-send-plane-fill"></i>
-              </button>
+          <div className="message-box p-2 flex-grow flex flex-col gap-2 overflow-auto max-h-full scrollbar-hide">
+
+            <div className="message max-w-64 flex flex-col p-2 bg-slate-50 w-fit rounded-md">
+              <small className="opacity-65 text-xs">example@gmail.com</small>
+              <p className="text-sm">Lorem ipsum dolor, sit amet consectetur Lorem ipsum dolor, sit amet consectetur</p>
             </div>
+
+            <div className="message ml-auto max-w-64 flex flex-col p-2 bg-slate-50 w-fit rounded-md">
+              <small className="opacity-65 text-xs">example@gmail.com</small>
+              <p className="text-sm">Lorem ipsum dolor, sit amet consectetur</p>
+            </div>
+
+          </div>
+
+          <div className="inputField w-full flex absolute bottom-0">
+            <input
+              className="p-2 px-4 border-none outline-none flex-grow"
+              type="text"
+              placeholder="Enter message"
+            />
+            <button className="px-5 bg-slate-950 text-white">
+              <i className="ri-send-plane-fill"></i>
+            </button>
           </div>
         </div>
       </section>
