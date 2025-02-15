f0586efe11060d5d776de3cf3e467fe2fb759d49
Muhammad-Haram <haramh643@gmail.com>
2025-02-15 18:01:00 +0500

    register page resolve

--- frontend/src/screens/Register.jsx
+++ frontend/src/screens/Register.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import axios from "../config/axios";
 import { UserContext } from "../context/user.context";
