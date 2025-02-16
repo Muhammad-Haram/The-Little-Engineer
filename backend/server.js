0361b6fcf0e944e8991018cb1088031ef6a1c093
Muhammad Haram <haramh643@gmail.com>
2025-02-16 21:37:16 +0500

    socket io error

--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,18 @@ import dotenv from 'dotenv';
 dotenv.config()
 import http from 'http'
 import app from './app.js';
+import { Server } from 'socket.io';
 
 const port = process.env.PORT || 8080;
 
 const server = http.createServer(app);
+const io = new Server(server)
+
+io.on('connection', socket => {
+    console.log('socket connection')
+    socket.on('event', data => { /* … */ });
+    socket.on('disconnect', () => { /* … */ });
+});
 
 server.listen(port, () => {
     console.log(`Server is running on port ${port}`)
